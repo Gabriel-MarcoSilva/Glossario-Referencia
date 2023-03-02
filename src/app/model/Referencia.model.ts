215d9ffd78e41243e5535378b2c4afd74b026e75
Gabriel-MarcoSilva <gabrielmarkolinosilva14@gmail.com>
2023-03-02 09:42:40 -0300

    feat: validações do formulário, chore: reescrita das variáveis que irão compor o model Referencia; assim como no cad-glossario foi aumentado a quantidade de caracteres que podem ser atribuídos a uma palavra a ser cadastrada;

--- src/app/model/Referencia.model.ts
+++ src/app/model/Referencia.model.ts
@@ -2,10 +2,16 @@ export class Referencia{
     constructor(
         public id: number,
         public author: String,
-        public type: String,
         public title: String,
-        public edition: String,
-        public published: number
+        public subtitle: String,
+        public numEdition: number,
+        public publisher: String,
+        public created_at: String,
+        public updated_at: String,
+        public publication: String,
+        public Pag: number,
+        public Vol: number,
+        public Year: number,
     ){
 
     }
--- src/app/modules/referencias/components/cad-referencias/cad-referencias.component.ts
+++ src/app/modules/referencias/components/cad-referencias/cad-referencias.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Referencia } from 'src/app/model/Referencia.model';
+import { ReferenciasService } from 'src/app/services/referencias/referencias.service';
 
 @Component({
   selector: 'app-cad-referencias',
@@ -7,25 +9,67 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./cad-referencias.component.css']
 })
 export class CadReferenciasComponent {
+  @Output() CadOk: EventEmitter<any> = new EventEmitter()
 
   public form!: FormGroup;
 
+  public referencia!: Referencia
 
   constructor(
-    private fb: FormBuilder
-  ){
+    private fb: FormBuilder,
+    private service: ReferenciasService
+  ) {
     this.form = this.fb.group({
-      keyWord:['', Validators.required],
-      description:['', Validators.required],
-      created_at:[''],
-      updated_at:[''],
+      author: ['', Validators.required],
+      title: ['', Validators.required],
+      subtitle: ['', Validators.required],
+      publication: ['', Validators.required],
+      Pag: [''],
+      Vol: [''],
+      numEdit: [''],
+      Year: [''],
+      publisher: ['', Validators.required],
+      created_at: [''],
+      updated_at: [''],
     })
   }
-  @Output() CadOk: EventEmitter<any> = new EventEmitter()
 
-  trade(){
+  onSubmit() {
+
+    const author = this.form.controls["author"].value
+    const publisher = this.form.controls["publisher"].value
+    const Year = this.form.controls["Year"].value
+    const numEdit = this.form.controls["numEdit"].value
+    const Vol = this.form.controls["Vol"].value
+    const Pag = this.form.controls["Pag"].value
+    const publication = this.form.controls["publication"].value
+    const subtitle = this.form.controls["subtitle"].value
+    const title = this.form.controls["title"].value
+    const created_at = this.date()
+
+
+    this.referencia = new Referencia(0, author, title, subtitle, numEdit, publisher, created_at, "--", publication, Pag, Vol, Year)
+  
+    console.log(this.referencia)
+
+  }
+
+
+  trade() {
     this.CadOk.emit()
 
+  }
+
+  date() {
+    const data = new Date();
+
+    const DD = String(data.getDate()).padStart(2, "0") //pega o dia
+    const MM = String(data.getMonth() + 1).padStart(2, "0") //pega o mês
+    const YY = data.getFullYear() //pega o ano
+
+    const date: String = `${DD}/${MM}/${YY}`
+
+    return date
   }
 
 }
